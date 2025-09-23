0482afee6517214a28e3ee6e0109864fb0c48b45
kskyasser <kossokoyasser@gmail.com>
2025-09-23 02:33:36 +0200

    Ajout de la page d'accueil index.php

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+?><!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>EcoRide</title>
+</head>
+<body>
+  <h1>Bienvenue sur EcoRide 🚗🌱</h1>
+  <p>Plateforme de covoiturage écoresponsable.</p>
+</body>
+</html>
+
